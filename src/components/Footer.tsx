d0a7501f8f41cf38e29842ad15f9621f4c4af8a1
HerbertSanches <herbertproduction@gmail.com>
2024-09-17 12:41:25 -0400

    style: ajuste altura footer4

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,23 +25,23 @@ export const Footer = () => {
   };
 
   return (
-    <nav className="flex flex-row fixed inset-x-0  bottom-0 items-center justify-between h-[40px] bg-azulEscuro text-white z-50">
+    <nav className="flex flex-row fixed inset-x-0 space-x-[15vh] bottom-0 items-center justify-center h-[45px] bg-azulEscuro text-white z-50">
       
-      <div onClick={() => handleNavigation('/dashboard')} className="ml-[10vh]">
-        <div className={`flex items-center justify-center h-[35px] w-[35px] rounded-full ${selected === 'dashboard' ? 'bg-green-400' : 'bg-transparent'}`}>
-          <Image src={Dinheiro} alt="Faturamento" className="h-[20px] w-[25px]" />
+      <div onClick={() => handleNavigation('/dashboard')} className="">
+        <div className={`flex items-center justify-center h-[40px] w-[40px] rounded-full ${selected === 'dashboard' ? 'bg-green-400' : 'bg-transparent'}`}>
+          <Image src={Dinheiro} alt="Faturamento" className="h-[30px] w-[35px]" />
         </div>
       </div>
 
       <div onClick={() => handleNavigation('/dashboardVendedor')}>
-        <div className={`flex items-center justify-center h-[35px] w-[35px] rounded-full ${selected === 'dashboardVendedor' ? 'bg-green-400' : 'bg-transparent'}`}>
-          <Image src={Usuario} alt="Vendedor" className="h-[20px] w-[15px]" />
+        <div className={`flex items-center justify-center h-[40px] w-[40px] rounded-full ${selected === 'dashboardVendedor' ? 'bg-green-400' : 'bg-transparent'}`}>
+          <Image src={Usuario} alt="Vendedor" className="h-[30px] w-[25px]" />
         </div>
       </div>
 
-      <div onClick={() => handleNavigation('/avisos')} className="mr-[10vh]">
-        <div className={`flex items-center justify-center h-[35px] w-[35px] rounded-full ${selected === 'avisos' ? 'bg-green-400' : 'bg-transparent'}`}>
-          <Image src={Notificacao} alt="Avisos" className="h-[20px] w-[20px]" />
+      <div onClick={() => handleNavigation('/avisos')}>
+        <div className={`flex items-center justify-center h-[40px] w-[40px] rounded-full ${selected === 'avisos' ? 'bg-green-400' : 'bg-transparent'}`}>
+          <Image src={Notificacao} alt="Avisos" className="h-[30px] w-[30px]" />
         </div>
       </div>
       
