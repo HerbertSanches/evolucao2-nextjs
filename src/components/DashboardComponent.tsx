4d14afbbb4be5660289d03c261b06b01718f0dfb
HerbertSanches <herbertproduction@gmail.com>
2024-09-18 17:17:37 -0400

    style: teste cor  back-ground

--- src/components/DashboardComponent.tsx
+++ src/components/DashboardComponent.tsx
@@ -207,13 +207,14 @@ const DashboardComponent: React.FC = () => {
   console.log(anoAtual)
   console.log(metaMesSelecionado)
   return (
-    <div className='w-full bg-branco'>
+    
+      
+     
+      <div className=' bg-blue-100 rounded-[8px] h-auto'>
       <h1 className='flex text-azulEscuro items-center justify-center font-bold text-xl mt-3 mb-3'>
         Dashboard Faturamento
       </h1>
-     
-      <div className='ml-3 mr-3 mt-3 mb-4 pb-3 bg-cinza rounded-[8px] h-auto'>
-        
+
         { mesAtual  !== mesSelecionado && mesAtual !== 0 ? ( 
           <Metas metaMes={metaMesSelecionado} mes={mesSelecionado-1} metaAno={metaAno} ano={anoSelecionado} />
         ) : <Metas metaMes={metaMes} mes={mesSelecionado-1} metaAno={metaAno} ano={anoSelecionado} />}
@@ -253,7 +254,7 @@ const DashboardComponent: React.FC = () => {
         </div>
 
       </div>
-    </div>
+  
     )
     
 };
