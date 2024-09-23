a4456e611b9399746798b98c3f06ed26280cced9
HerbertSanches <herbertproduction@gmail.com>
2024-09-23 17:59:49 -0400

    feat: adicionado Pordutos mais vendidos

--- src/app/login/[company]/page.tsx
+++ src/app/login/[company]/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import "../../globals.css";
-import LoginTela from '../../../components/LoginTela';
+import LoginTela from '../../../components/loginTela';
 
 const Login: React.FC = () =>  {
   
