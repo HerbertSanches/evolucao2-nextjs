e690d82f53c8b21262c6e58b5ed391380ba58853
HerbertSanches <herbertproduction@gmail.com>
2024-09-24 11:31:00 -0400

    style: ajuste mais vendidos

--- src/components/ProdutosMaisVendidos.tsx
+++ src/components/ProdutosMaisVendidos.tsx
@@ -8,19 +8,36 @@ interface produtosMaisVendidos {
 }
 
 const ProdutosMaisVendidos = () => {
+
+
     const anoAtual = new Date().getFullYear();
     const mesAtual = new Date().getMonth()+1;
     const anoAtualString = anoAtual.toString();
     const [mesSelecionado, setMesSelecionado] = useState(mesAtual);
     const [anoSelecionado, setAnoSelecionado] = useState<string>(anoAtualString); 
     const [maisVendidos, setMaisVendidos] = useState<produtosMaisVendidos[]>([]);
+    const [quantidadeValorSelecionado, setQuantidadeValorSelecionado] = useState(0);
     
-    const tipoFiltro = 1;
+    const meses = [
+        { nome: 'Janeiro', valor: 1 },
+        { nome: 'Fevereiro', valor: 2 },
+        { nome: 'Março', valor: 3 },
+        { nome: 'Abril', valor: 4 },
+        { nome: 'Maio', valor: 5 },
+        { nome: 'Junho', valor: 6 },
+        { nome: 'Julho', valor: 7 },
+        { nome: 'Agosto', valor: 8 },
+        { nome: 'Setembro', valor: 9 },
+        { nome: 'Outubro', valor: 10 },
+        { nome: 'Novembro', valor: 11 },
+        { nome: 'Dezembro', valor: 12 },
+    ];
+
     useEffect(() => {
         const idEmpresa = localStorage.getItem('idEmpresa');
         const tokenHeader = localStorage.getItem('token');
         const fetchDataProdutosMaisVendidos = async () => {
-            const responseMetaMesAno = await api.get(`venda/listarmaisvendidos/${idEmpresa}/${tipoFiltro}/${anoSelecionado}/${mesSelecionado}`,{
+            const responseMetaMesAno = await api.get(`venda/listarmaisvendidos/${idEmpresa}/${quantidadeValorSelecionado}/${anoSelecionado}/${mesSelecionado}`,{
                 headers: {
                     'Authorization': `Bearer ${tokenHeader}`
                 }
@@ -32,10 +49,53 @@ const ProdutosMaisVendidos = () => {
         };
 
         fetchDataProdutosMaisVendidos();
-    }, [])
+    }, [mesSelecionado, anoSelecionado, quantidadeValorSelecionado])
 
 
+
+
+
+    const handleClick = (mes: number) => {
+        setMesSelecionado(mes);
+        console.log(`Mês selecionado: ${mes}`);
+    };
+
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setQuantidadeValorSelecionado(Number(event.target.value)); // Convertendo o valor para número
+        console.log(`Ano selecionado: ${event.target.value}`);
+    };
+
+    console.log(mesSelecionado)
+    console.log(anoSelecionado)
+    console.log(quantidadeValorSelecionado)
     return(
+        <>
+        <div className="flex space-x-3 overflow-x-auto py-2 bg-azulEscuro ">
+          {meses.map((mes) => (
+            <button
+                key={mes.valor}
+                className={`px-4 min-w-24 h-7 bg-branco rounded-full p-1 items-center font-semibold text-14 justify-center ${
+                    mesSelecionado === mes.valor
+                    ? 'bg-green-400 text-white '
+                    : 'text-azulEscuro'
+                }`}
+                onClick={() => handleClick(mes.valor)}
+                data-mes={mes.valor} // Adiciona o valor do mês como um data attribute
+            >
+                {mes.nome}
+            </button>
+            ))}
+
+          <select value={anoSelecionado} onChange={handleSelectChange} className='justify-center rounded-full p-1 items-center font-semibold bg-branco mr-1 text-azulEscuro cursor-pointer '>
+              <option value={anoAtual}>
+              Vendas de {anoAtual}
+              </option>
+              <option value={anoAtual - 1}>Vendas de {anoAtual - 1}</option>
+              <option value={anoAtual - 2}>Vendas de {anoAtual - 2}</option>
+              <option value={anoAtual - 3}>Vendas de {anoAtual - 3}</option>
+          </select>
+        </div>
+        
         <main>
             <h1 className='flex text-azulEscuro items-center justify-center font-bold text-xl mt-3 mb-3'>
                 Dashboard 10 Mais Vendidos
@@ -46,20 +106,25 @@ const ProdutosMaisVendidos = () => {
                 <div className="ml-2 mr-2 mt-2 pb-2 bg-branco rounded-md shadow-md">
                     <div className="flex flex-row ml-3 mr-3 p-1 mt-1 justify-between text-azulClaro items-center text-base font-semibold border-b-3 border-solid border-cinza">
                         <h1 className="ml-3">Produtos Mais Vendidos</h1>
-                        <h1 className="mr-1">Quantidade</h1>
+                        <select value={quantidadeValorSelecionado} onChange={handleSelectChange} className='justify-center rounded-full p-1 items-center font-semibold bg-branco mr-1 text-azulClaro cursor-pointer '>
+                            <option value={0}>
+                            Quantidade
+                            </option>
+                            <option value={1}>Valor</option>
+                        </select>
                     </div>
                     {maisVendidos.map((maisVendidos) => (
-                        <div className="flex items-center justify-between border-b border-solid ml-2 mr-2 p-1 mt-1 border-cinza shadow-md">
+                        <div className="flex items-center justify-between border-b border-solid ml-2 mr-2 p-1 mt-1 border-cinza shadow-md"
+                        key={maisVendidos.pr_descricao}
+                        >
                             <span
-                                key={maisVendidos.pr_descricao}
                                 title={maisVendidos.pr_descricao}
                                 className="flex px-4 min-w-24 h-7 rounded-full items-center text-14 justify-center truncate text-center"
                             >
                                 {maisVendidos.pr_descricao}
                             </span>
 
-                            <span
-                                key={maisVendidos.pr_descricao}
+                            <span 
                                 className="flex w-auto px-4 min-w-24 h-7 rounded-full p-1 items-center text-14 justify-center text-center"
                             >
                                 {maisVendidos.total}
@@ -69,7 +134,7 @@ const ProdutosMaisVendidos = () => {
                 </div>
             </div>
         </main>
-
+        </>
             
         
     )
